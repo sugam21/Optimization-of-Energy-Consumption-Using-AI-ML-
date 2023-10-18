7c293e4b3cc90de1b3738973f3112d086ebdaaa3
Sugam <sugams342@gmail.com>
2023-10-18 08:18:22 +0530

    removed the unnecessary and added the pickle

--- notebooks/Path.py
+++
@@ -1,8 +0,0 @@
-class Path:
-    def get_path():
-        return "/home/sugam/Work/20-29 ImageProcessing/22 Projects/Optimization of Energy Using AIML/data/raw/Building_59/Bldg59_clean data"
-    def get_files():
-        import os
-        data_files = os.listdir(path_to_data)
-        return data_files
-        
